75951185a309ba1fb678f03e40aedc31034d9215
taylorngo <taylorngo3@gmail.com>
2022-03-08 15:37:50 -0600

    commit 1 temp up/down broken

--- app/src/main/java/com/example/coolercontrol/CoolerFragment.java
+++ app/src/main/java/com/example/coolercontrol/CoolerFragment.java
@@ -4,17 +4,74 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-
 public class CoolerFragment extends Fragment {
+    TextView showCountTextView;
+
+    private void countUp(View view) {
+        //Get the value of the text view
+        String countString = showCountTextView.getText().toString();
+        //Convert value to a number and increment it
+        Integer count = Integer.parseInt(countString);
+        count++;
+        //Display the new value in text view
+        showCountTextView.setText(count.toString());
+    }
+
+    private void countDwn(View view) {
+        //Get the value of the text view
+        String countString = showCountTextView.getText().toString();
+        //Convert value to a number and increment it
+        Integer count = Integer.parseInt(countString);
+        count--;
+        //Display the new value in text view
+        showCountTextView.setText(count.toString());
+    }
+
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView( LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_cooler, container, false);
+        //Get the count text view
+        showCountTextView = fragmentFirstLayout.findViewById(R.id.textview_first);
         return inflater.inflate(R.layout.fragment_cooler, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        view.findViewById(R.id.refresh_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast myToast = Toast.makeText(getActivity(), "System Updated!", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+
+        view.findViewById(R.id.down_count_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countDwn(view);
+            }
+        });
+        view.findViewById(R.id.up_count_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countUp(view);
+            }
+        });
+
+    }
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
     }
 }
