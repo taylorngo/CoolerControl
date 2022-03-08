ce0c48b320f44d6dc0db40cd1a16fae61179a055
taylorngo <taylorngo3@gmail.com>
2022-03-08 17:57:29 -0600

    commit 2 user manual

--- app/src/main/java/com/example/coolercontrol/MainActivity.java
+++ app/src/main/java/com/example/coolercontrol/MainActivity.java
@@ -55,6 +55,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new GPSFragment()).commit();
                 break;
+            case R.id.nav_help:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HelpFragment()).commit();
+                break;
             case R.id.nav_share:
                 Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
                 break;
